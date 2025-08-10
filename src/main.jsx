565074d9a3184ddac6ab5ec6e37320e04d502693
yida <sthofyidatsai.design@gmail.com>
2025-08-10 23:08:59 +0800

    appcss

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './styles/reset.css';
 import './styles/global.css';
 import './styles/fonts.css';
+import './App.css';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
