24400ff33513322c7e1bd3c1b54af1848648f91e
yida <sthofyidatsai.design@gmail.com>
2025-09-07 18:46:54 +0800

    header

--- src/components/HeaderVertical.jsx
+++ src/components/HeaderVertical.jsx
@@ -4,6 +4,10 @@ import "./HeaderVertical.css";
 import projectsData from "../data/projects.json";
 import arrowIcon from "../assets/icons/arrow_outward_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg";
 import arrowUpIcon from "../assets/icons/arrow_circle_up_44dp_2D2D2D_FILL0_wght300_GRAD0_opsz48.svg";
+import menuNumber1 from "../assets/icons/meun-number-1.svg";
+import menuNumber2 from "../assets/icons/meun-number-2.svg";
+import menuNumber3 from "../assets/icons/meun-number-3.svg";
+import menuNumber4 from "../assets/icons/meun-number-4.svg";
 import MarqueeText from "./MarqueeText";
 
 export default function Header() {
@@ -147,25 +151,33 @@ export default function Header() {
         <div className="menu-section">
           <nav className="navigation">
             <ul>
-              <li>
-                <Link to="/" onClick={toggleMenu}>
-                  HOME
+              <li className="nav-item">
+                <Link to="/" onClick={toggleMenu} className="nav-link">
+                  <span className="nav-text">HOME</span>
+                  <img src={menuNumber1} alt="1" className="nav-number" />
                 </Link>
+                <div className="nav-divider"></div>
               </li>
-              <li>
-                <Link to="/projects" onClick={toggleMenu}>
-                  WORK
+              <li className="nav-item">
+                <Link to="/projects" onClick={toggleMenu} className="nav-link">
+                  <img src={menuNumber2} alt="2" className="nav-number" />
+                  <span className="nav-text">WORK</span>
                 </Link>
+                <div className="nav-divider"></div>
               </li>
-              <li>
-                <Link to="/about" onClick={toggleMenu}>
-                  ABOUT
+              <li className="nav-item">
+                <Link to="/about" onClick={toggleMenu} className="nav-link">
+                  <span className="nav-text">ABOUT</span>
+                  <img src={menuNumber3} alt="3" className="nav-number" />
                 </Link>
+                <div className="nav-divider"></div>
               </li>
-              <li>
-                <Link to="/contact" onClick={toggleMenu}>
-                  CONTACT
+              <li className="nav-item">
+                <Link to="/contact" onClick={toggleMenu} className="nav-link">
+                  <img src={menuNumber4} alt="4" className="nav-number" />
+                  <span className="nav-text">CONTACT</span>
                 </Link>
+                <div className="nav-divider"></div>
               </li>
             </ul>
           </nav>
