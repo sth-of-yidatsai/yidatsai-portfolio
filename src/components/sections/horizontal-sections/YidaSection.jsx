534daa7c594c8f3b3bfae7ec4d3fb5f551da999b
yida <81798905+sth-of-yidatsai@users.noreply.github.com>
2025-09-22 08:01:13 +0800

    Home_AboutMeSection

--- src/components/sections/horizontal-sections/YidaSection.jsx
+++ src/components/sections/horizontal-sections/YidaSection.jsx
@@ -13,6 +13,27 @@ export default function YidaSection({ index }) {
   return (
     <div className={`hs-section yida-section hs-section-${index}`}>
       <div className="yida-grid">
+        <div className="yida-name-block">
+          <div className="yida-name-block-1"></div>
+          <div className="yida-image-frame">
+            <div className="yida-image-wrapper">
+              <img
+                src="/images/projects/project-005/project-005-img-12.webp"
+                alt="yida"
+                className={`yida-image ${
+                  isScrolling
+                    ? isInHorizontalSection
+                      ? `scroll-horizontal-${horizontalDirection}`
+                      : `scroll-vertical-${verticalDirection}`
+                    : ""
+                }`}
+              />
+            </div>
+          </div>
+          <div className="yida-name-block-2">
+            <p className="yida-name">Yida Tsai</p>
+          </div>
+        </div>
         <div className="yida-image-block yida-image-block-1">
           <div className="yida-image-frame">
             <div className="yida-image-wrapper">
@@ -32,10 +53,27 @@ export default function YidaSection({ index }) {
         </div>
         <div className="yida-text-block yida-text-block-1">
           <div className="yida-about-me">
-            <span className="about-me">
+            <p className="about-me about-me-label">About</p>
+            <div className="divider-vertical" />
+            <p className="about-me about-me-text">
               Independent multidisciplinary designer, based in Taipei, Taiwan —
               working with brands and institutions across Asia–Pacific.
-            </span>
+            </p>
+            <div className="yida-image-frame">
+              <div className="yida-image-wrapper">
+                <img
+                  src="/images/projects/project-005/project-005-img-06.webp"
+                  alt="yida"
+                  className={`yida-image ${
+                    isScrolling
+                      ? isInHorizontalSection
+                        ? `scroll-horizontal-${horizontalDirection}`
+                        : `scroll-vertical-${verticalDirection}`
+                      : ""
+                  }`}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
