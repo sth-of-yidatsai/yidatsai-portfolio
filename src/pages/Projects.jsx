7cf6ada77b612b02589ee480a34d77893d2bae9d
yida <sthofyidatsai.design@gmail.com>
2026-03-17 19:31:32 +0800

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import SelectWork from "../components/sections/projectsPage/SelectWork";
+import SelectWork from "../components/sections/ProjectsPage/SelectWork";
 import "./Projects.css";
 
 export default function Projects() {
